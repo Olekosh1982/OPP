5fa24015c0d24b0cbec381c0897228889bbd1ffd
ВашÐOlesia Dumaeva <@superdumaev1978@gmail.com>
2023-08-10 11:32:12 +0200

    23

--- src/lesson_18/Lesson_18.java
+++ src/lesson_18/Lesson_18.java
@@ -1,4 +1,21 @@
 package lesson_18;
 
+import java.util.Arrays;
+
 public class Lesson_18 {
+    public static void main(String[] args) {
+     //   int[] array=new int[2];
+     //   int var =10;
+      //  array[0]=var;
+      //  System.out.println(Arrays.toString(array));
+
+      //  int var2=25;
+      //  array[1]=25;
+        RubberArray rb=new RubberArray();
+        rb.printArray();
+        rb.add(11);
+        rb.add(22);
+        rb.add(33);
+        rb.printArray();
+    }
 }
---
+++ src/lesson_18/RubberArray.java
@@ -0,0 +1,29 @@
+package lesson_18;
+
+import java.util.Arrays;
+
+public class RubberArray {
+    int[] array;
+    public RubberArray(){
+        this.array=new int[0];
+    }
+    public void add(int value){
+        extractarray();
+        array[array.length-1]=value;
+    }
+    public void extractarray(){
+        array= Arrays.copyOf(array,array.length+1);
+    }
+    public void printArray(){
+        System.out.print("[");
+        for(int i=0;i< array.length;i++){
+            System.out.print(array[i]);
+            if (i< array.length-1){
+                System.out.print(";");
+            }
+        }
+        System.out.println("]");
+        System.out.println();
+    }
+
+}
