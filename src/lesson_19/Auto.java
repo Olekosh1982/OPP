7ef936a84095bdbeda58837116202805fa83c2da
Olesia Dumaeva <141398748+Olekosh1982@users.noreply.github.com>
2023-08-11 11:06:37 +0200

    auto

---
+++ src/lesson_19/Auto.java
@@ -0,0 +1,10 @@
+package lesson_19;
+
+public class Auto {
+    public void gas(){
+        System.out.println("");
+    }
+    public void breakMe(){
+        System.out.println("");
+    }
+}
---
+++ src/lesson_19/Cat.java
@@ -0,0 +1,39 @@
+package lesson_19;
+
+public class Cat {
+    String name;
+   // int age;
+   double weight;
+    public Cat(String catName, double weight1){
+     this.name=catName;
+     this.weight=weight1;
+    }
+    void  startTraining(int laps){
+        System.out.println("Start training s "+laps+" krugami");
+        for ( int i=0;i<laps;i++){
+
+                System.out.println("Круг №"+(i+1));
+                this.run();
+                System.out.println("end lap\n");
+        }
+    }
+    void  run(){
+        while (weight<=2){
+            System.out.println("Я слишком голоден"+weight);
+            eat();
+            System.out.println("my weight"+weight);
+
+        }
+        System.out.println("Я бегу, ох как тяжело!"+weight);
+        weight-=2;
+    }
+    void  eat(){
+        System.out.println("Я покушал!!");
+        weight++;
+    }
+    void whoAmI(){
+        System.out.println("Я котик "+name+" и я вешу "+weight);
+    }
+
+
+}
