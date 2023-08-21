52eb766c6cc690e8f5b71e8ede8b38a64857d5ec
Olesia Dumaeva <141398748+Olekosh1982@users.noreply.github.com>
2023-08-21 13:43:21 +0200

    polimorfism12

--- src/lesson_25/OurStringBuilder.java
+++ src/lesson_25/OurStringBuilder.java
@@ -2,21 +2,26 @@ package lesson_25;
 
 public class OurStringBuilder {
     public static void main(String[] args) {
-        String str="absd";
+        String str = "abcd";
         System.out.println(str.substring(2));
         System.out.println(str);
-        str="frtu";
-        //StringBuilder - создания изменяемых строк
-        //StringBuilder()
-        //StringBuilder(String)
-        //StringBuilder(int length)
+        str = "frtu";
 
-        // метод append(String s)-добавляет строку к имеющейся
+        // StringBuilder  - исполльзуется для создания изменяемых строк
+        // конструкторы:
+        /*
+        StringBuilder() -пустой стрингбилдер
+        StringBuilder(String) - стрингбилдер cj cnhjrjq String
+        StringBuilder(int length) - пустой стрингбилдер размера length
 
-        StringBuilder sb=new StringBuilder("Hello");
+        // метод append(String s) - добавляет строку к имеющейся
+
+         */
+
+        StringBuilder sb = new StringBuilder("Hello");
         System.out.println(sb);
-        sb.append("").append("Java");
+        sb.append(" ").append("Java");
         System.out.println(sb);
-
     }
 }
+
